54a3cb7fd0d0d3abd6a5c90c4b76f5882017bdb1
achrefbs <siachrefboularess@gmail.com>
2021-05-15 14:03:41 +0100

    init

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:shopapp/models/product.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // final Product product;
+  // ProductDetailScreen(this.product);
+  static const routeName = '/product_detail';
+  @override
+  Widget build(BuildContext context) {
+    final product = ModalRoute.of(context).settings.arguments as Product;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shopapp/models/product.dart';
+import 'package:shopapp/screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  ProductItem(this.product);
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product);
+        },
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          product.title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
